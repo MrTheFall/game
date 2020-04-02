72a1942021a191e894d6a9f43404f6c0515e4b1d
MrTheFall <mrthefall@yandex.ru>
2020-04-02 19:13:18 +0700

    Add ammo counter and reloading. Need fix.
    Something wrong with ammo counter after reloading.

--- Game/Assets/Gun.cs
+++ Game/Assets/Gun.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
 
 public class Gun : MonoBehaviour
 {
@@ -10,12 +12,37 @@ public class Gun : MonoBehaviour
 
     public int bulletSpeed;
     public float despawnTime = 3.0f;
+    public int maxAmmo = 5;
+    public int currentAmmo;
+    public float reloadTime = 1f;
+    private bool isReloading = false;
+    public Text currentAmmoDisplay;
 
     public bool shootAble = true;
-    public float waitBeforeNextShot = 0.25f;
+    public float waitBeforeNextShot = 0.4f;
 
-    private void Update()
+    void Start()
     {
+        currentAmmo = maxAmmo;
+    }
+
+    void OnEnable()
+    {
+        isReloading = false;
+    }
+
+    void Update()
+    {
+        currentAmmoDisplay.text = currentAmmo.ToString();
+
+        if (isReloading)        
+            return;
+
+        if (currentAmmo <= 0)
+        {
+            StartCoroutine(Reload());
+            return;
+        }
         if (Input.GetKey(KeyCode.Mouse0))
         {
             if (shootAble)
@@ -34,9 +61,19 @@ public class Gun : MonoBehaviour
     }
     void Shoot()
     {
+        currentAmmo--;
+        Debug.Log(currentAmmo);
         var bullet = Instantiate(theBullet, barrelEnd.position, barrelEnd.rotation);
         bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * bulletSpeed;
 
         Destroy(bullet, despawnTime);
+    }
+
+    IEnumerator Reload()
+    {
+        Debug.Log("Reloading..");
+        yield return new WaitForSeconds(reloadTime);
+        currentAmmo = maxAmmo;
+        isReloading = false;
     }
 }
