40a093887f3fce48da706dbd66bb53a544eab076
MrTheFall <mrthefall@yandex.ru>
2020-04-02 14:35:28 +0700

    Add destroying bullet on trigger
    Also fixed bullets hitting player's gun.

---
+++ Game/Assets/BulletAction.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletAction : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider hitInfo)
+    {
+        if(hitInfo.gameObject.name != "Gun")
+        {
+            Destroy(gameObject); //Here must be enemy hit logic
+            Debug.Log(hitInfo.gameObject);
+        }
+    }
+}
