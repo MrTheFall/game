dde241829ed946a13efc0a4e499398993bb79648
MrTheFall <mrthefall@yandex.ru>
2020-06-06 01:29:01 +0700

    Update: main menu. Add: server list, create match menu

--- Game/Assets/Launcher.cs
+++ Game/Assets/Launcher.cs
@@ -2,11 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using Photon.Realtime;
+using System.Security.Cryptography;
+using UnityEngine.UI;
 
 namespace FPSGame
 {
     public class Launcher : MonoBehaviourPunCallbacks
     {
+        public InputField usernameField;
+        public InputField roomnameField;
+        public Text mapValue;
+        public Text modeValue;
+        public Slider maxPlayersSlider;
+        public Text maxPlayersValue;
+
+        public GameObject tabMain;
+        public GameObject tabRooms;
+        public GameObject tabCreate;
+
+        public GameObject buttonRoom;
+
+        private List<RoomInfo> roomList;
+
         public void Awake()
         {
             PhotonNetwork.AutomaticallySyncScene = true;
@@ -16,6 +34,8 @@ namespace FPSGame
         public override void OnConnectedToMaster()
         {
             Debug.Log("Joined");
+
+            PhotonNetwork.JoinLobby();
             base.OnConnectedToMaster();
         }
 
@@ -48,8 +68,88 @@ namespace FPSGame
 
         public void Create()
         {
-            PhotonNetwork.CreateRoom("");
+            RoomOptions options = new RoomOptions();
+            options.MaxPlayers = (byte)maxPlayersSlider.value;
+
+            // will use this later
+            ExitGames.Client.Photon.Hashtable properties = new ExitGames.Client.Photon.Hashtable();
+            properties.Add("map", 0);
+            options.CustomRoomProperties = properties;
+
+            PhotonNetwork.CreateRoom(roomnameField.text, options);
             Debug.Log("Created new room");
+        }
+
+        public void ChangeMap()
+        {
+
+        }
+        
+        public void ChangeMode()
+        {
+
+        }
+
+        public void ChangeMaxPlayersSlider(float t_value)
+        {
+            maxPlayersValue.text = Mathf.RoundToInt(t_value).ToString();
+        }
+
+        public void TabCloseAll()
+        {
+            tabMain.SetActive(false);
+            tabRooms.SetActive(false);
+            tabCreate.SetActive(false);
+        }   
+
+        public void TabOpenRooms()
+        {
+            TabCloseAll();
+            tabRooms.SetActive(true);
+        }
+
+        public void TabOpenMain()
+        {
+            TabCloseAll();
+            tabMain.SetActive(true);
+        }
+
+        public void TabOpenCreate()
+        {
+            TabCloseAll();
+            tabCreate.SetActive(true);
+        }
+
+            public void ClearRoomList()
+        {
+            Transform content = tabRooms.transform.Find("Scroll View/Viewport/Content");
+            foreach (Transform a in content) Destroy(a.gameObject);
+        }
+
+        public override void OnRoomListUpdate(List<RoomInfo> p_list)
+        {
+            roomList = p_list;
+            ClearRoomList();
+
+            Transform content = tabRooms.transform.Find("Scroll View/Viewport/Content");
+
+            foreach (RoomInfo a in roomList)
+            {
+                GameObject newRoomButton = Instantiate(buttonRoom, content) as GameObject;
+
+                newRoomButton.transform.Find("Name").GetComponent<Text>().text = a.Name;
+                newRoomButton.transform.Find("Players").GetComponent<Text>().text = a.PlayerCount + " / " + a.MaxPlayers;
+
+                newRoomButton.GetComponent<Button>().onClick.AddListener(delegate { JoinRoom(newRoomButton.transform); });
+            }
+
+            base.OnRoomListUpdate(roomList);
+        }
+
+        public void JoinRoom(Transform p_button)
+        {
+            string t_roomName = p_button.Find("Name").GetComponent<Text>().text;
+            PhotonNetwork.JoinRoom(t_roomName);
         }
 
         public void StartGame()
