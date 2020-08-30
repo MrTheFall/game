fd5880b5e13a08e7c164bf9eb224d2a40e7ff06e
MrTheFall <mrthefall@yandex.ru>
2020-08-30 23:22:35 +0700

    Add some changes

---
+++ Game/Assets/Bomb.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    private bool onBombPlant = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if (onBombPlant)
+            {
+                //PlantBomb(); !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "BombPlant")
+        {
+            onBombPlant = true;
+        }
+    }
+    void OnTriggerExit(Collider collider)
+    {
+        if (collider.tag == "BombPlant")
+        {
+            onBombPlant = false;
+        }
+    }
+}
